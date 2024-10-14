326790d52e943fa6db19fec6fb20218c5ccfc12f
wellwellgood <kky6108@naver.com>
2024-10-14 17:57:41 +0900

    scroll.js 2024-10-14

--- scroll.js
+++ scroll.js
@@ -1,10 +1,44 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  let button = $('button');
+  let close = $(".close")
+  let box = $(".box");
 
-  $(".").mouseover(function () {
-    $(this).addClass("active");
-
-  });
-  $(".").mouseout(function () {
+  $(button).click(function () {
     $(this).removeClass("active");
-  });
-});
+    $(box).addClass("on");
+    $(box).css("visibility","visible");
+  })
+  $(close).click(function () {
+    $(box).removeClass("on");
+    $(box).css("visibility","hidden");
+  })
+
+  $("#btn1").click(function () {
+    $("#box1").show();
+    $("#box2").hide();
+    $("#box3").hide();
+  })
+  $("#btn2").click(function () {
+    $("#box2").show();
+    $("#box1").hide();
+    $("#box3").hide();
+  })
+  $("#btn3").click(function () {
+    $("#box3").show();
+    $("#box1").hide();
+    $("#box2").hide();
+  })
+})
+
+
+
+let container = document.querySelector(".role");
+let totalCircle = 80;
+let angleStep = 360 / totalCircle;
+for (let i = 0; i < totalCircle; i++){
+    let circle = document.createElement("div");
+    circle.classList.add("circleimg");
+    let rotation = i * angleStep;
+    circle.style.transform = `rotate(${rotation}deg) translate(165px)`;
+    container.appendChild(circle);
+}
